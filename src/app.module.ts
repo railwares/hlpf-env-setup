12b799df8e30c0796638523b75b74ec1e5e5c5f1
railwares <umyeswater@gmail.com>
2026-03-22 23:42:59 +0200

    init nestjs project with db and redis connection

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+ 
+@Module({
+  imports: [
+	ConfigModule.forRoot({ isGlobal: true }),
+	TypeOrmModule.forRoot({
+  	type: 'postgres',
+  host: process.env.POSTGRES_HOST as string,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER as string,
+  password: process.env.POSTGRES_PASSWORD as string,
+  database: process.env.POSTGRES_DB as string,
+  entities: [],
+  synchronize: true, // тільки для розробки!
+	}),
+CacheModule.registerAsync({
+  	isGlobal: true,
+  	useFactory: async () => ({
+    	store: await redisStore({
+      	socket: {
+        	host: process.env.REDIS_HOST,
+        	port: parseInt(process.env.REDIS_PORT, 10),
+      	},
+    	}),
+    	ttl: 60 * 1000, // 60 секунд у мілісекундах
+  	}),
+	}),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
