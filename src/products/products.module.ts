c970ba839c0239eddc36aacd4c7bbfe89ef9f6e1
railwares <umyeswater@gmail.com>
2026-04-20 22:02:38 +0300

    add JwtAuthGuard, RolesGuard, protect endpoints

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { AuthModule } from '../auth/auth.module';
  
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+	TypeOrmModule.forFeature([Product]),
+	AuthModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
 export class ProductsModule {}
+
